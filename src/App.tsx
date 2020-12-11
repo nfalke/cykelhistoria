9767d33d1207d4216ae6fbbebebeb4aa4c7c64d8
Nils <nfalke@gmail.com>
2020-12-11 18:49:09 +0100

    adding react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import React, { useState } from "react";
 import { Header, bikeDataInterface } from "./components/Header";
 import { Main } from "./components/Main";
 import { Footer } from "./components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   const [bikeData, setBikeData] = useState<bikeDataInterface>(null);
 
   return (
-    <>
-      <Header bikeData={bikeData} setBikeData={(data) => setBikeData(data)} />
-      <Main bikeData={bikeData} />
-      <Footer />
-    </>
+    <Router>
+      <Route path="/*">
+        <Header bikeData={bikeData} setBikeData={(data) => setBikeData(data)} />
+        <Main bikeData={bikeData} />
+        <Footer />
+      </Route>
+    </Router>
   );
 };
 
