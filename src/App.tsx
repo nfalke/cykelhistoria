6b339e24adb2fd6e5506a4faef5ea01cf4ea7e52
Nils <nfalke@gmail.com>
2020-12-11 22:04:57 +0100

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,16 @@ import React, { useState } from "react";
 import { Header, bikeDataInterface } from "./components/Header";
 import { Main } from "./components/Main";
 import { Footer } from "./components/Footer";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   const [bikeData, setBikeData] = useState<bikeDataInterface>(null);
 
   return (
-    <Router>
-      <Route path="/*">
-        <Header bikeData={bikeData} setBikeData={(data) => setBikeData(data)} />
-        <Main bikeData={bikeData} />
-        <Footer />
-      </Route>
-    </Router>
+    <>
+      <Header bikeData={bikeData} setBikeData={(data) => setBikeData(data)} />
+      <Main bikeData={bikeData} />
+      <Footer />
+    </>
   );
 };
 
